4d9566fe1578b7d84a509152f98404b0e3addf98
Roberto Lopez <jrlopez@gmail.com>
2013-09-26 16:48:37 -0500

    Objetos referenciados

--- src/com/itd/Geolocalizacion/MyActivity.java
+++ src/com/itd/Geolocalizacion/MyActivity.java
@@ -1,9 +1,15 @@
 package com.itd.Geolocalizacion;
 
 import android.app.Activity;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
+    private LocationManager locManager;
+    private LocationListener locListener;
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +17,13 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView latitud = (TextView)findViewById(R.id.lblPosLatitud);
+        TextView longitud = (TextView)findViewById(R.id.lblPosLongitud);
+        TextView presicion = (TextView)findViewById(R.id.lblPresicion);
+
+        Button prende = (Button)findViewById(R.id.btnActualizar);
+        Button apaga = (Button)findViewById(R.id.btnApagar);
+        TextView estado = (TextView)findViewById(R.id.lblEstado);
+
     }
 }
